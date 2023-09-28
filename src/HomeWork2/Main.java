01b833e6aa0e0506ad1529ab05034bea4edd7516
agarkovanatoliy <132005412+agarkovanatoliy@users.noreply.github.com>
2023-09-28 21:00:06 +0300

    Решение ДЗ 2

---
+++ src/HomeWork2/Main.java
@@ -0,0 +1,19 @@
+package HomeWork2;
+
+public class Main {
+    public static void main(String[] args) {
+//        Human name = new Human();
+//        name.setName("Aleksandr");
+//
+//        name.setMakeOrder();
+//        name.setTakeOrder();
+//        System.out.println(name);
+
+        Human name = new Human();
+        name.setName("Николай");
+
+        Market market = new Market();
+
+        market.update(name);
+    }
+}
